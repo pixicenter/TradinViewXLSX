d487b2c93c8a9a45907ccb5fc720a0b09fabbd24
pixicenter <46507604+pixicenter@users.noreply.github.com>
2025-02-18 14:35:55 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import uvicorn
 from app.main import app  # Asigură-te că 'app.main' este corect
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=4000)
 # DACA vrei să deschizi automat un browser:
-    import webbrowser
-    webbrowser.open("http://127.0.0.1:8000")
+#    import webbrowser
+#    webbrowser.open("http://127.0.0.1:8000")
